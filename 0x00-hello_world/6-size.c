b00055d065cc59fff8fab672837db1b779ae6fe6
Timothy Adeleke <dlktimothy@gmail.com>
2022-04-07 15:58:41 -0700

    Update 6-size,c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /*
  *
- * Main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -19,5 +19,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
 	return (0);
 }
