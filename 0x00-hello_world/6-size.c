cd39e1bf4ce1eba352a864b732fb160f004b5258
Timothy Adeleke <dlktimothy@gmail.com>
2022-04-07 15:39:39 -0700

    Add 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ *
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Size of a char: 1 byte(s)");
+	puts("Size of an int: 4 byte(s)");
+	puts("Size of a long int: 4 byte(s)");
+	puts("Size of a long long int: 8 byte(s)");
+	puts("Size of a float: 4 byte(s)");
+	return (0);
+}
