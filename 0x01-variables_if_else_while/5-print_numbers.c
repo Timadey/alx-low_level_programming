a7a320b397930b882b8a56daf946e661d56b9a97
Timothy Adeleke <dlktimothy@gmail.com>
2022-04-08 09:18:07 -0700

    Add 5 print numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Declaration */
+	int i;
+
+	/* Body */
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	};
+	putchar('\n');
+	return (0);
+}
