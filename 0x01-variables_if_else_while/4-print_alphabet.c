da6fa31475eb820d0bf29e1e71f9a2015f1790cd
Timothy Adeleke <dlktimothy@gmail.com>
2022-04-08 09:09:00 -0700

    Add 4 print alphabets.c

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,25 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Declaration */
+	char letter = 'a';
+	int i;
+
+	/* Body */
+	for (i = 0; i < 26; i++)
+	{
+		if (letter != 'e' || letter != 'q')
+		{
+			putchar(letter);
+		};
+	};
+	putchar ('\n');
+	return (0);
+}
