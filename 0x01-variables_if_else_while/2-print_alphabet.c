deeb0731aa2d012a4366231c5b5d976aefb56e89
Timothy Adeleke <dlktimothy@gmail.com>
2022-04-08 08:41:44 -0700

    Add 2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+	int i;
+	for (i = 0; i < 23; i++)
+	{
+		putchar (letter);
+	};
+	putchar ('\n');
+	return (0);
+}
