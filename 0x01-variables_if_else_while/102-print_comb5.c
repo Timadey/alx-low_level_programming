d91f1fede97efdfbc6d037226a3dcb009ca5cecf
Timothy Adeleke <dlktimothy@gmail.com>
2022-04-10 07:19:18 -0700

    Updated 102 print comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,54 +1,52 @@
-
 #include <stdio.h>
-/**
- * main - main Entry
- * Description: print all possible combination of two digits
- * Return: Always 0 (Success)
+/*
+ * main - The starting point of the program
+ * Description: Write a program that prints
+ * all possible combinations of two two-digit numbers.
+ *
+ * The numbers should range from 0 to 99
+ * The two numbers should be separated by a space
+ * All numbers should be printed with two digits.
+ *  1 should be printed as 01
+ * The combination of numbers must be separated by comma,
+ * followed by a space
+ * The combinations of numbers should be printed in ascending order
+ * 00 01 and 01 00 are considered as
+ * the same combination of the numbers 0 and 1
+ * You can only use the putchar function
+ *  (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar eight times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ *
+ * Return: 0, void or non-zero value when an error occurs
  */
+
 int main(void)
 {
-	/* Declaration */
+	/* Declarations */
 	int first_digit;
 	int sec_digit;
-	int third_digit;
-	int fourth_digit;
-	int first_part;
-	int sec_part;
-	int total = 4 * ('9');
 
-	for (first_digit = '0'; first_digit <= '9'; first_digit++)
+	for (first_digit = 0; first_digit < 100; first_digit++)
 	{
-		for (sec_digit = '0'; sec_digit <= '9'; sec_digit++)
+		for (sec_digit = 0; sec_digit < 100; sec_digit++)
 		{
-			for (third_digit = '0'; third_digit <= '9'; third_digit++)
+			if (first_digit < sec_digit)
 			{
-			for (fourth_digit = '0'; fourth_digit <= '9'; fourth_digit++)
-			{
-				first_part = first_digit + sec_digit;
-				sec_part = third_digit + fourth_digit;
-			if (first_part < sec_part &&
-					first_part != sec_part)
-				{
-				putchar (first_digit);
-				putchar (sec_digit);
+				putchar ((first_digit / 10) + '0');
+				putchar ((first_digit % 10) + '0');
 				putchar (' ');
-				putchar (third_digit);
-				putchar (fourth_digit);
-				if ((first_part + sec_part) != total)
-		/*
-		 *	if (first_digit != '9' && sec_digit != '9'
-		*				&& third_digit != '9l
-		*				&& fourth_digit != '9'
-		*/
-					{
+				putchar ((sec_digit / 10) + '0');
+				putchar ((sec_digit % 10) + '0');
+				if (first_digit < 98)
+				{
 					putchar (',');
 					putchar (' ');
-					};
 				};
 			};
-			};
+		};
 	};
-	};
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
