19fa6d1d114a01c893dd2cf2627e546b9939d30d
Timothy Adeleke <dlktimothy@gmail.com>
2022-04-12 06:15:10 -0700

    Modify 0putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,6 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <unistd.h>
+int _putchar(char c);
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
 #endif
