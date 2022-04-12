52a611d44b952e2e5c36ca7d08e67fa371119583
Timothy Adeleke <dlktimothy@gmail.com>
2022-04-12 06:42:13 -0700

    Add 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - the starting point of the program
+ * Description: Write a function that prints the alphabet,
+ *  in lowercase, followed by a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
