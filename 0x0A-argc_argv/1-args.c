7ec4a972135f21771db88950ee1151e53a97d23d
Timothy Adeleke <dlktimothy@gmail.com>
2022-05-02 11:48:01 -0700

    Add new files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main (int argc, __attribute__((unused))char *argv[])
+{
+printf ("%d\n", argc);
+return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main (int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		int mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+		return (0);
+	};
+}
