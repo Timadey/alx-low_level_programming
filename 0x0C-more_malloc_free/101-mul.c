5f78bb4ca1f79f06c5cf0cb6ce620989748553b9
Timothy Adeleke <dlktimothy@gmail.com>
2022-05-09 15:31:35 -0700

    Modify 101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * check_dig - check if a char is digit
+ * @arr: the array containing the char
+ * @len: length of the array
+ * Return: void
+ */
+void check_dig(char *arr, int len)
+{
+	int i = 0;
+
+	for (; i < len; i++)
+	{
+		if (!isdigit(arr[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		};
+	};
+}
+/**
+ * main -Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		exit(98);
+	}
+	check_dig(argv[1], strlen(argv[1]);
+	check_dig(argv[2], strlen(argv[2]);
+	return (0);
+}
