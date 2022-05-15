0b113cad715d98ee2e790e34e1a0994073a0c121
Timothy Adeleke <dlktimothy@gmail.com>
2022-05-15 00:58:39 -0700

    Fix 0bug 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
+#include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
