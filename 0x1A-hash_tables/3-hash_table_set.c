1f6ecdb144c854429e29f8b15916aff7f53e85da
Joshua <jo4christ48@yahoo.com>
2022-07-29 12:38:40 +0100

    [Add] add functionality to add element to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t **array;
+	hash_node_t *new_node;
 	hash_node_t *node;
 
 	if (ht == NULL || key == NULL)
@@ -26,11 +27,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node == NULL)
 		return (0);
 
-	new_node->key = key;
-	new_node->value = value;
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 	new_node->next = NULL;
 
-	if ((array + index) == NULL)
+	if (array[index] == NULL)
 	{
 		array[index] = new_node;
 		return (1);
@@ -38,9 +39,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		node = array[index];
-		while (node->next != NULL)
-			node = node->next;
-		node->next = new_node;
+		array[index] = new_node;
+		new_node->next = node;
 		return (1);
 	}
 }
