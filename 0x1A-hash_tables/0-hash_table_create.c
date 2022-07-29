c8416849ac3d22b0e872f741fb3d4c46665da450
Timadey <dlktimothy@gmail.com>
2022-07-29 13:08:32 +0100

    [Fix] Array size Only `hash` table was malloc'd with a size of one before

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,9 +7,9 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	hash_table_t *new_table = malloc(sizeof(hash_table_t) * size);
 
-	if (new_table == NULL)
+	if (new_table == NULL || size == 0)
 		return (NULL);
 
 	new_table->size = size;
