e774a1b4162a177bdfc5318e93f5dfb3997c51fc
Timadey <dlktimothy@gmail.com>
2022-07-30 14:44:45 +0100

    [Fix] malloc error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,16 +7,22 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	hash_table_t *new_table;
 
-	if (new_table == NULL || size == 0)
+	if (size == 0)
+		return (NULL);
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t) * size);
+	new_table->array = malloc(size * sizeof(hash_node_t *));
 
 	if (new_table->array == NULL)
+	{
+		free(new_table);
 		return (NULL);
+	}
 
 	return (new_table);
 }
