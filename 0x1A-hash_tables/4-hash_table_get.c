6c19bc017b76a676078ce9d95081e3fe0b80d393
Timadey <dlktimothy@gmail.com>
2022-08-01 12:47:31 +0100

    [Add] Implement print feature

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_tabls_get - retrieves a value associated with a key
+ * hash_table_get - retrieves a value associated with a key
  * @ht: hash table you want to look into
  * @key: the key you are looking for
  * Return: the value associatd with the element, or NULL if key couldn't be found
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,46 @@
+#include "hash_tables.h"
+
+/**
+ * hash_tables_print - prints a hash table
+ * @ht: the hash table
+ * Return: prints the key/value in the order that they appear on tha hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int size;
+	unsigned long int index;
+	hash_node_t *node;
+	unsigned long int count;
+	hash_node_t **array;
+
+	if (ht == NULL)
+		printf("{}\n");
+	else
+	{
+		size = ht->size;
+		array = ht->array;
+		index = 0;
+
+		printf("{");
+		count = 0;
+		while (index < size)
+		{
+			node = array[index];
+
+			if (node != NULL)
+			{
+				if (count > 0)
+					printf(", ");
+				else
+					count++;
+				while (node != NULL)
+				{
+					printf("'%s': '%s'", node->key, node->value);
+					node = node->next;
+				}
+			}
+			index++;
+		}
+		printf("}\n");
+	}
+}
